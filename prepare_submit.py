20eb89a0da1c9644140e4271fa718b6e93691308
yuchia329 <yuchia.chang@outlook.com>
2025-05-03 13:59:02 -0700

    update prepare_submit.py

--- prepare_submit.py
+++ prepare_submit.py
@@ -6,26 +6,31 @@ import os
 import sys
 import zipfile
 
-required_files = {'run_llama.py',
-                  'llama.py',
-                  'optimizer.py',
-                  'classifier.py',
-                  'rope.py',
-                  'generated-sentence-temp-0.txt',
-                  'generated-sentence-temp-1.txt',
-                  'sst-dev-prompting-output.txt',
-                  'sst-test-prompting-output.txt',
-                  'sst-dev-finetuning-output.txt',
-                  'sst-test-finetuning-output.txt',
-                  'cfimdb-dev-prompting-output.txt',
-                  'cfimdb-test-prompting-output.txt',
-                  'cfimdb-dev-finetuning-output.txt',
-                  'cfimdb-test-finetuning-output.txt'}
+required_files = {
+    "run_llama.py",
+    "llama.py",
+    "optimizer.py",
+    "classifier.py",
+    "rope.py",
+    "generated-sentence-temp-0.txt",
+    "generated-sentence-temp-1.txt",
+    "sst-dev-prompting-output.txt",
+    "sst-test-prompting-output.txt",
+    "sst-dev-finetuning-output.txt",
+    "sst-test-finetuning-output.txt",
+    "cfimdb-dev-prompting-output.txt",
+    "cfimdb-test-prompting-output.txt",
+    "cfimdb-dev-finetuning-output.txt",
+    "cfimdb-test-finetuning-output.txt",
+}
 
-optional_files = {'sst-dev-advanced-output.txt',
-                  'sst-test-advanced-output.txt',
-                  'cfimdb-dev-advanced-output.txt',
-                  'cfimdb-test-advanced-output.txt'}
+optional_files = {
+    "sst-dev-advanced-output.txt",
+    "sst-test-advanced-output.txt",
+    "cfimdb-dev-advanced-output.txt",
+    "cfimdb-test-advanced-output.txt",
+}
+
 
 def check_file(file: str, check_aid: str):
     global required_files
@@ -33,7 +38,7 @@ def check_file(file: str, check_aid: str):
     target_prefix = None
     # --
     inside_files = set()
-    with zipfile.ZipFile(file, 'r') as zz:
+    with zipfile.ZipFile(file, "r") as zz:
         # --
         print(f"Read zipfile {file}:")
         zz.printdir()
@@ -46,47 +51,67 @@ def check_file(file: str, check_aid: str):
                 target_prefix, _ = info.filename.split("/", 1)
                 target_prefix = target_prefix + "/"
             # --
-            assert info.filename.startswith(target_prefix), \
-                'There should only be one top-level dir (with your andrew id as the dir-name) inside the zip file.'
-            ff = info.filename[len(target_prefix):]
+            assert info.filename.startswith(
+                target_prefix
+            ), "There should only be one top-level dir (with your andrew id as the dir-name) inside the zip file."
+            ff = info.filename[len(target_prefix) :]
             inside_files.add(ff)
         # --
     # --
     required_files -= inside_files
     combined_files = required_files | optional_files
     combined_files -= inside_files
-      
-    assert len(required_files) == 0, f"Some required files are missing: {required_files}"
+
+    assert (
+        len(required_files) == 0
+    ), f"Some required files are missing: {required_files}"
     # --
-    assert target_prefix[:-1] == check_aid, f"AndrewID mismatched: {target_prefix[:-1]} vs {check_aid}"
-    print(f"Read zipfile {file}, please check that your andrew-id is: {target_prefix[:-1]}")
+    assert (
+        target_prefix[:-1] == check_aid
+    ), f"AndrewID mismatched: {target_prefix[:-1]} vs {check_aid}"
+    print(
+        f"Read zipfile {file}, please check that your andrew-id is: {target_prefix[:-1]}"
+    )
     print(f"And it contains the following files: {sorted(list(inside_files))}")
     # -- OPTIONAL CHECK --
-    assert len(combined_files) in [4,0], f"[Optional check] Some of your advanced outputs are missing: {combined_files}"
+    assert len(combined_files) in [
+        4,
+        0,
+    ], f"[Optional check] Some of your advanced outputs are missing: {combined_files}"
     # --
+
 
 def main(path: str, aid: str):
     aid = aid.strip()
     if os.path.isdir(path):
-        with zipfile.ZipFile(f"{aid}.zip", 'w') as zz:
+        with zipfile.ZipFile(f"{aid}.zip", "w") as zz:
             for root, dirs, files in os.walk(path):
-                if '.git' in root or '__pycache__' in root:
-                    continue  # ignore some parts
+                dirs[:] = [
+                    d for d in dirs if not d.startswith(".") and d != "__pycache__"
+                ]
                 for file in files:
-                    if file.endswith(".zip"):
+                    if (
+                        file.endswith(".zip")
+                        or file.startswith(".")
+                        or file.endswith(".pt")
+                        or file.startswith("__")
+                    ):
                         continue
                     ff = os.path.join(root, file)
                     rpath = os.path.relpath(ff, path)
                     zz.write(ff, os.path.join(".", aid, rpath))
                     if rpath in required_files:
                         required_files.remove(rpath)
-        assert len(required_files) == 0, breakpoint()
+        assert (
+            len(required_files) == 0
+        ), f"Unable to get all required files {required_files}"
         # --
         print(f"Submission zip file created from DIR={path} for {aid}: {aid}.zip")
-        check_file(f'{aid}.zip', aid)
+        check_file(f"{aid}.zip", aid)
     else:  # directly check
         check_file(path, aid)
     # --
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(*sys.argv[1:])
